8ad9326057d5fcfa2bc46454f5ede0735694109e
gemtunde <gemtunde@gmail.com>
2022-02-27 19:50:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.scss';
+
+import { Header, About, Footer, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+
+     
+    </div>
+  );
+}
+
+export default App;
